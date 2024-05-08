afc2b03bb79870ca757b7d38a5c3457e10142db3
Rushabh <rushabhkhandhar38@.com>
2024-05-08 11:09:03 +0530

    fixed small bugs

--- models/Post.js
+++ models/Post.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
     content: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Corrected reference to 'User' model
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }, // Corrected reference to 'User' model
     createdAt: { type: Date, default: Date.now },
     likes: { type: Number, default: 0 },
     comments: [{ 
         text: String,
-        author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Corrected reference to 'User' model
+        author: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }, // Corrected reference to 'User' model
         createdAt: { type: Date, default: Date.now }
     }]
 });
@@ -15,3 +15,4 @@ const postSchema = new mongoose.Schema({
 const postModal = mongoose.model("Post", postSchema);
 
 export default postModal;
+
