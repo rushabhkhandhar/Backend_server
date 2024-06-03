5d0f7626927a63995d4b69cb128bcf5483fec73f
Rushabh <rushabhkhandhar38@.com>
2024-06-03 20:51:57 +0530

    updated backend

--- Backend/config/connectdb.js
+++ Backend/config/connectdb.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+     mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
